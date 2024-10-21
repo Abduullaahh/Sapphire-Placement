de73d1ce389821f283cd339a78d62a6866052a58
Abduullaahh <hafizabdullahtahir@gmail.com>
2024-10-21 21:30:00 +0500

    Navbar Border Added

--- components/custom/layout.tsx
+++ components/custom/layout.tsx
@@ -76,7 +76,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/sapphire-placement-small-logo.svg" />
       </Head>
 
-      <header className="bg-white shadow-sm fixed top-0 left-0 right-0 z-50">
+      <header className="bg-white shadow-sm fixed top-0 left-0 right-0 z-50 border-b-2 border-[#0095DA]">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between py-3">
             <div className="flex items-center">
